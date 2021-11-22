a652ee7ff03934b734b0c48633c0b20187b65102
Victor Ambrozi <victorambrozi.dev@gmail.com>
2021-11-22 11:01:02 -0300

    logica do cronometro

--- src/pages/Cronometro.js
+++ src/pages/Cronometro.js
@@ -1,33 +1,78 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, Image, TouchableOpacity, View } from 'react-native';
 
+let timer = null;
+let seg = 0, min = 0, hour = 0;
 
 export const Cronometro = () => {
+  const [number, setNumber] = useState('00:00:00');
+  const [button, setButton] = useState("INICIAR");
+  const [lastTime, setLastTime] = useState(null);
+
+  const start = () => {
+    if (timer !== null) {
+      clearInterval(timer);
+      timer = null;
+
+      setButton('INICIAR');
+    } else {
+      timer = setInterval(() => {
+        seg++;
+
+        if (seg === 60) {
+          seg = 0;
+          min++;
+        };
+
+        if (min === 60) {
+          min = 0;
+          hour++;
+        };
+
+        let format = `${hour < 10 ? '0' + hour : hour}:${min < 10 ? '0' + min : min}:${seg < 10 ? '0' + seg : seg}`;
+        setNumber(format);
+      }, 1000);
+      setButton('PAUSAR');
+    }
+  };
+  const restart = () => {
+    if(timer !== null) {
+      clearInterval(timer);
+      timer = null;
+    }
+    setNumber('00:00:00');
+    seg = 0; min = 0; hour = 0;
+    setButton("INICIAR");
+    setLastTime(number);
+  };
+
   return (
     <>
       <Image
         source={require('../../assets/crono.png')}
       />
-      <Text style={styles.timer}> 00:00:00</Text>
+      <Text style={styles.timer}>{number}</Text>
 
       <View style={styles.btnControl}>
-        <TouchableOpacity>
+        <TouchableOpacity onPress={start}>
           <View style={styles.btnArea}>
-            <Text style={styles.btnText}>Iniciar</Text>
+            <Text style={styles.btnText}>{button}</Text>
           </View>
         </TouchableOpacity>
 
-        <TouchableOpacity>
+        <TouchableOpacity onPress={restart}>
           <View style={styles.btnArea}>
             <Text style={styles.btnText}>Limpar</Text>
           </View>
         </TouchableOpacity>
       </View>
 
-      <Text style={styles.time}>Ultimo tempo 00:03:50</Text>
+      <Text style={styles.time}>
+        {lastTime ? `Último tempo: ${lastTime}` : null}
+      </Text>
     </>
   )
-}
+};
 
 const styles = StyleSheet.create({
   timer: {
@@ -63,5 +108,6 @@ const styles = StyleSheet.create({
     fontSize: 18,
     color: '#FFF',
     marginTop: 15,
+    fontStyle: 'italic'
   }
 })
